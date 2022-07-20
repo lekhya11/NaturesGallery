2ac761cfe70c756da3aff5b9244477740f759070
k.lekhya <107404763+lekhya11@users.noreply.github.com>
2022-07-20 12:19:55 +0530

    Update ThumbnailItem.js

--- ThumbnailItem.js
+++ ThumbnailItem.js
@@ -3,12 +3,24 @@
 import './index.css'
 
 const ThumbnailItem = props => {
-  const {thumbnailUrl, thumbnailAltText} = props
+  const {imageDetails, updateImageId, isActive} = props
+  const {id, thumbnailUrl, thumbnailAltText} = imageDetails
+  const imageClassName = isActive ? `thumbnail active` : `thumbnail`
+
+  const onClickImage = () => {
+    updateImageId(id)
+  }
 
   return (
     <>
       <li className="thumbnail-image">
-        <img src={thumbnailUrl} alt={thumbnailAltText} />
+        <button className="image-button" type="button" onClick={onClickImage}>
+          <img
+            src={thumbnailUrl}
+            alt={thumbnailAltText}
+            className={imageClassName}
+          />
+        </button>
       </li>
     </>
   )
